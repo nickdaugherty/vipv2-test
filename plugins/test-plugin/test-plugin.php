10be71ff0b93dc3f9d7980a3087a6ad3be783475
Nick Daugherty <ndaugherty987@gmail.com>
2015-12-21 16:30:39 -0700

    Add test plugin

---
+++ plugins/test-plugin/test-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: This is a test
+Plugin URI: 
+Description: Test test test
+Version: 3.0.1
+Author: Automattic
+Author URI: http://automattic.com/wordpress-plugins/
+License: GPLv2 or later
+Text Domain: test
+*/
