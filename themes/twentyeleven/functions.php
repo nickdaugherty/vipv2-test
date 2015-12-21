3dfb7dc4938fad02c61cd5c887d2b8069eb23dc2
Nick Daugherty <ndaugherty987@gmail.com>
2015-12-21 16:30:59 -0700

    Enable test plugin

--- themes/twentyeleven/functions.php
+++ themes/twentyeleven/functions.php
@@ -43,6 +43,7 @@ if ( ! isset( $content_width ) )
 	$content_width = 614;
 
 wpcom_vip_load_plugin( 'fieldmanager' );
+wpcom_vip_load_plugin( 'test-plugin' );
 
 /*
  * Tell WordPress to run twentyeleven_setup() when the 'after_setup_theme' hook is run.
